2678842927c464ebf4fc1024ae60a0b4621d469e
Leonardo-Boss <70913810+Leonardo-Boss@users.noreply.github.com>
2021-11-17 21:40:17 -0300

    save app data files in correct directory

--- app.py
+++ app.py
@@ -2,17 +2,20 @@ import time
 import pickle
 import os
 
+import appdirs
 from flask import Flask, render_template, request
 import webview
 
 from moodle_api import Moodle
 
 app = Flask(__name__)
+temp = os.path.join(appdirs.user_config_dir('moodle_tarefas', False), 'temp')
+
 
 @app.route('/')
 def index():
-    if os.path.exists('temp'):
-        with open('temp', 'rb') as file:
+    if os.path.exists(temp):
+        with open(temp, 'rb') as file:
             form = pickle.load(file)
         moodle = Moodle(form[0], form[1])
         events = moodle.get_events()[0]['data']['events']
@@ -23,7 +26,7 @@ def index():
 def login():
     if request.form['remember'] == 'true':
         form = [request.form['username'], request.form['password']]
-        with open('temp', 'wb') as file:
+        with open(temp, 'wb') as file:
             pickle.dump(form, file)
     moodle = Moodle(request.form['username'], request.form['password'])
     events = moodle.get_events()[0]['data']['events']
@@ -31,8 +34,8 @@ def login():
 
 @app.route('/logout', methods=['POST'])
 def logout():
-    if os.path.exists('temp'):
-        os.remove('temp')
+    if os.path.exists(temp):
+        os.remove()
     return render_template('login.html')
 
 
